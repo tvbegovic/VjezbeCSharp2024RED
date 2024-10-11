82d3d78b87193f8cc1037d2f47b7b428f2bc23ce
prof <prof@KAT33>
2024-10-11 12:56:51 +0200

    Add project files.

---
+++ ProsjekOcjena/Program.cs
@@ -0,0 +1,20 @@
+﻿try
+{
+	Console.Write("Ocjena C: ");
+	string unos = Console.ReadLine();
+	int ocjenaC = int.Parse(unos);
+	Console.Write("Ocjena C++: ");
+	unos = Console.ReadLine();
+	int ocjenaCPlus = int.Parse(unos);
+	Console.Write("Ocjena C#: ");
+	unos = Console.ReadLine();
+	int ocjenaCSharp = int.Parse(unos);
+	//stavljamo 3.0 da natjeramo C# da dijeli kao realni broj jer će inače 
+	//dijeliti cjelobrojno
+	double prosjek = (ocjenaC + ocjenaCPlus + ocjenaCSharp) / 3.0;
+	Console.WriteLine($"Prosjek ocjena je {prosjek:N2}");
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"Dogodila se pogreška. Tekst pogreške: {ex.Message}");
+}
---
+++ TocnoVrijeme/Program.cs
@@ -0,0 +1,5 @@
+﻿//stara varijanta placeholdera
+Console.WriteLine("Dobar dan. Točno vrijeme je {0}", DateTime.Now);
+//novija verzija placeholdera
+Console.WriteLine($"Dobar dan. Točno vrijeme je {DateTime.Now}");
+
