8d6ac57c556f27635d87c103701d7cbaa844f1ff
Tvrtko <tvrtkobegovic@gmail.com>
2024-10-21 10:55:13 +0200

    Petlje

---
+++ Djelitelji/Program.cs
@@ -0,0 +1,32 @@
+﻿bool nastavi = true;
+
+do
+{
+    Console.Write("Unesite broj (1-100.000): ");
+    string unos = Console.ReadLine();
+    if(string.IsNullOrEmpty(unos))
+        nastavi = false;
+    else
+    {
+        bool ok = int.TryParse(unos, out int broj);
+        if (!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        if (broj < 1 || broj > 100000)
+        {
+            Console.WriteLine("Broj je izvan raspona");
+            continue;
+        }
+        //da izbjegnemo overflow kod 100.000, int mijenjamo u long
+        //int zbroj = 0;
+        long zbroj = 0;
+        for (int i = 1; i <= broj; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+                zbroj += i;
+        }
+        Console.WriteLine($"Zbroj brojeva od 1 do {broj} djeljivih sa 3 ili 5 je {zbroj:N0}"); 
+    } 
+} while (nastavi);
--- PogadanjeBroja/Program.cs
+++ PogadanjeBroja/Program.cs
@@ -1,40 +1,49 @@
 ﻿using System.Runtime;
+bool nastavi = true;
 
-Console.WriteLine("Zamislio sam broj od 1-10. Probaj ga pogoditi i možda dobiješ čokoladu");
-Random random = new Random();
-int zamisljeniBroj = random.Next(1, 11);
-Console.Write("Koji sam broj zamislio? ");
-string unos = Console.ReadLine();
-bool ok = int.TryParse(unos, out int broj);
-if(!ok)
+do
 {
-    Console.WriteLine("Broj je u pogrešnom formatu");
-    return;
-}
-if(broj < 1 ||  broj > 10)
-{
-    Console.WriteLine("Broj je izvan raspona");
-    return;
-}
-int razlika = Math.Abs(zamisljeniBroj - broj);
-string tekst = string.Empty;
-switch(razlika)
-{
-    case 0:
-        tekst = "Sjajno, pogodili ste broj. Imate čokoladu";
-        break;
-    case 1:
-    case 2:
-    case 3:
-        tekst = $"Broj je {zamisljeniBroj}. Jako dobro. Osvajate pola čokolade";
-        break;
-    case 4:
-    case 5:
-    case 6:
-        tekst = $"Broj je {zamisljeniBroj}. Solidno. Dobili ste jedno rebro čokolade";
-        break;
-    default:
-        Console.WriteLine("Više sreće drugi put");
-        break;
-}
-Console.WriteLine(tekst);
+    Console.WriteLine("Zamislio sam broj od 1-10. Probaj ga pogoditi i možda dobiješ čokoladu");
+    Random random = new Random();
+    int zamisljeniBroj = random.Next(1, 11);
+    Console.Write("Koji sam broj zamislio? ");
+    string unos = Console.ReadLine();
+    if(string.IsNullOrEmpty(unos))
+        nastavi = false;
+    else
+    {
+        bool ok = int.TryParse(unos, out int broj);
+        if (!ok)
+        {
+            Console.WriteLine("Broj je u pogrešnom formatu");
+            continue;
+        }
+        if (broj < 1 || broj > 10)
+        {
+            Console.WriteLine("Broj je izvan raspona");
+            continue;
+        }
+        int razlika = Math.Abs(zamisljeniBroj - broj);
+        string tekst = string.Empty;
+        switch (razlika)
+        {
+            case 0:
+                tekst = "Sjajno, pogodili ste broj. Imate čokoladu";
+                break;
+            case 1:
+            case 2:
+            case 3:
+                tekst = $"Broj je {zamisljeniBroj}. Jako dobro. Osvajate pola čokolade";
+                break;
+            case 4:
+            case 5:
+            case 6:
+                tekst = $"Broj je {zamisljeniBroj}. Solidno. Dobili ste jedno rebro čokolade";
+                break;
+            default:
+                Console.WriteLine("Više sreće drugi put");
+                break;
+        }
+        Console.WriteLine(tekst);  
+    }
+} while (nastavi);
