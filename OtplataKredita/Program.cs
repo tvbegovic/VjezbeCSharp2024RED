c122bcaaaadbeb7f939015fbfab36aafb790cfef
Tvrtko <tvrtkobegovic@gmail.com>
2024-10-28 10:55:38 +0100

    Otplata kredita i izlazni

---
+++ OtplataKredita/Program.cs
@@ -0,0 +1,62 @@
+﻿void IzracunParametara(double iznos, double stopa, int brojGodina, 
+    out double anuitet, out double iznosKamata)
+{
+    if (iznos < 0 || stopa < 0 || brojGodina < 0)
+    {
+        throw new ArgumentException("Parametri moraju biti pozitivni");
+    }
+    double r = Kamatnjak(stopa);
+    int n = brojGodina * 12;
+    anuitet = iznos * Math.Pow(r,n) * (r-1) / (Math.Pow(r,n) - 1);
+    iznosKamata = anuitet * n - iznos;
+}
+
+double Kamatnjak(double stopa)
+{
+    return 1 + stopa / 100 / 12;
+}
+
+bool nastavi = true;
+do
+{
+    Console.Write("Glavnica: ");
+    string unos = Console.ReadLine();
+    if(string.IsNullOrEmpty(unos) )
+        nastavi = false;
+    else
+    {
+        bool ok = double.TryParse(unos, out double glavnica);
+        if(!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        Console.Write("Stopa: ");
+        unos = Console.ReadLine();
+        ok = double.TryParse(unos, out double stopa);
+        if (!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        Console.Write("Broj godina: ");
+        unos = Console.ReadLine();
+        ok = int.TryParse(unos, out int brojGodina);
+        if (!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        try
+        {
+            IzracunParametara(glavnica, stopa, brojGodina, out double anuitet,
+                out double iznosKamata);
+            Console.WriteLine($"Za glavnicu {glavnica:N2}, stopu {stopa}% i broj godina {brojGodina} anuitet je {anuitet:N2} i iznos kamata je {iznosKamata:N2}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Pogreška. {ex.Message}");
+        }
+    }
+}
+while (nastavi);
---
+++ PrimjerIzlazniParametri/Program.cs
@@ -0,0 +1,27 @@
+﻿//1. korak - funkcija koja vraća jednu vrijednost
+double Kvadrat(double broj)
+{
+    return broj * broj;
+}
+//2. korak - funkcija vraća 2 vrijednosti
+double KvadratKub(double broj, out double kub)
+{
+    double kvadrat = broj * broj;
+    kub = broj * broj * broj;
+    return kvadrat;
+}
+//3. korak - funkcija vraća obje vrijednosti preko out parametara
+void KvadratKub2(double broj, out double kvadrat, out double kub)
+{
+    kvadrat = broj * broj;
+    kub = Math.Pow(broj, 3);
+}
+
+//Pozivamo funkciju kvadrat
+double kvadrat = Kvadrat(340);
+Console.WriteLine(kvadrat);
+kvadrat = KvadratKub(340, out double kubBroja);
+Console.WriteLine($"Kvadrat je {kvadrat} a kub je {kubBroja}");
+
+KvadratKub2(340, out double kvadrat2, out double kub2);
+Console.WriteLine($"Kvadrat je {kvadrat2} a kub je {kub2}");
