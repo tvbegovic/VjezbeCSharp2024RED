4279ab164b36222ba8f5f3a26f8bb4b1972aa035
Tvrtko <tvrtkobegovic@gmail.com>
2024-11-11 10:56:50 +0100

    Klase 1. dio

---
+++ Klase/Glavna.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Klase
+{
+    public partial class Glavna : Form
+    {
+        
+        public Glavna()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            Igra igra1 = new Igra();
+            igra1.Naziv = txtNaziv.Text;
+            igra1.Opis = txtOpis.Text;
+            igra1.Vrsta = txtVrsta.Text;
+            string unos = txtDatum.Text;
+            bool ok = DateTime.TryParse(unos, out DateTime datum);
+            if (!ok)
+            {
+                MessageBox.Show("Datum u pogrešnom formatu");
+                return;
+            }
+            igra1.DatumIzdavanja = datum;
+            unos = txtCijena.Text;
+            ok = double.TryParse(unos, out double cijena);
+            if (!ok)
+            {
+                MessageBox.Show("Cijena u pogrešnom formatu");
+                return;
+            }
+            igra1.Cijena = cijena;
+            igra1.Izdavac = txtIzdavac.Text;
+            MessageBox.Show("Igra unesena");
+
+        }
+    }
+}
---
+++ Klase/Igra.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klase
+{
+    internal class Igra
+    {
+        private string naziv, opis, vrsta, izdavac;
+        private DateTime datumIzdavanja;
+        private double cijena;
+
+        public void UpisiNaziv(string naziv)
+        {
+            if (string.IsNullOrEmpty(naziv))
+                throw new ArgumentException("Naziv ne smije biti prazan");
+            this.naziv = naziv;
+        }
+        public string CitajNaziv()
+        {
+            return this.naziv;
+        }
+
+        //public properties
+        //get-set par za naziv
+        public string Naziv
+        {
+            get 
+            { 
+                return this.naziv; 
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Naziv ne smije biti prazan");
+                this.naziv = value;
+            }
+        }
+
+        public string Opis
+        {
+            get
+            {
+                return this.opis;
+            }
+            set
+            {
+                this.opis = value;
+            }
+        }
+        public string Vrsta
+        {
+            get
+            {
+                return this.vrsta;
+            }
+            set
+            {
+                this.vrsta = value;
+            }
+        }
+        public DateTime DatumIzdavanja
+        {
+            get
+            {
+                return this.datumIzdavanja;
+            }
+            set
+            {
+                this.datumIzdavanja = value;
+            }
+        }
+
+        public double Cijena
+        {
+            get
+            {
+                return this.cijena;
+            }
+            set
+            {
+                this.cijena = value;
+            }
+        }
+        public string Izdavac
+        {
+            get
+            {
+                return this.izdavac;
+            }
+            set
+            {
+                this.izdavac = value;
+            }
+        }
+
+    }
+}
