021ab5d1c7c9582dcc7c46841c6b6bb21b388c13
Tvrtko <tvrtkobegovic@gmail.com>
2025-01-10 13:03:55 +0100

    gameshopweb ver1

---
+++ GameshopWeb/Controllers/GameController.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace GameshopWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public GameController(IConfiguration configuration) 
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet("genres")]
+        public List<Genre> GetGenres()
+        {
+            using (var conn = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                return conn.Query<Genre>("SELECT * FROM Genre").ToList();
+            }
+        }
+    }
+}
---
+++ GameshopWeb/DomainClasses.cs
@@ -0,0 +1,51 @@
+﻿namespace GameshopWeb
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Order
+    {
+        public int Id { get; set; }
+        public int? IdUser { get; set; }
+        public int? IdEmployee { get; set; }
+        public DateTime? DateOrdered { get; set; }
+        public DateTime? DateSent { get; set; }
+    }
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int? IdOrder { get; set; }
+        public int? IdGame { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? Unitprice { get; set; }
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool? Admin { get; set; }
+        public string City { get; set; }
+    }
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int? IdGenre { get; set; }
+        public int? IdPublisher { get; set; }
+        public decimal? Price { get; set; }
+        public int? IdDeveloper { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string Image { get; set; }
+    }
+}
