15fc85e116e182ff234c3053fbc70f62d57124b9
Tvrtko <tvrtkobegovic@gmail.com>
2025-01-13 10:55:40 +0100

    Ostale funkcije početne stranice

--- GameshopWeb/Controllers/GameController.cs
+++ GameshopWeb/Controllers/GameController.cs
@@ -1,7 +1,7 @@
 ﻿using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace GameshopWeb.Controllers
 {
@@ -22,6 +22,48 @@ namespace GameshopWeb.Controllers
             using (var conn = new SqlConnection(configuration.GetConnectionString("connString")))
             {
                 return conn.Query<Genre>("SELECT * FROM Genre").ToList();
+            }
+        }
+
+        [HttpGet("companies")]
+        public List<Company> GetCompanies()
+        {
+            using(var conn = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                return conn.Query<Company>("SELECT * FROM Company").ToList();
+            }
+        }
+
+        [HttpGet("bygenre/{id}")]
+        public List<Game> GetByGenre(int id)
+        {
+            using (var conn = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                return conn.Query<Game>("SELECT * FROM Game WHERE idGenre = @id", new { id }).ToList();
+            }
+        }
+
+        [HttpGet("bycompany/{id}")]
+        public List<Game> GetByCompany(int id)
+        {
+            using (var conn = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                return conn.Query<Game>("SELECT * FROM Game WHERE idPublisher = @id OR idDeveloper = @id", new { id }).ToList();
+            }
+        }
+
+        [HttpGet("search/{text}")]
+        public List<Game> Search(string text)
+        {
+            using (var conn = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                string textParam = $"%{text}%";
+                return conn.Query<Game>(
+                    @"SELECT * FROM Game
+                      WHERE Title LIKE @textParam", new
+                    {
+                        textParam
+                    }).ToList();
             }
         }
     }
--- GameshopWeb/Program.cs
+++ GameshopWeb/Program.cs
@@ -17,6 +17,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
