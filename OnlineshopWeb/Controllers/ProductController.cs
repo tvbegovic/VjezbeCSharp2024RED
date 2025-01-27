3ffcc05a5a6581aebf648c23e00caeeba8b608a8
Tvrtko <tvrtkobegovic@gmail.com>
2025-01-27 10:59:03 +0100

    onlineshop

---
+++ OnlineshopWeb/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace OnlineshopWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public ProductController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet("")]
+        public List<Product> GetProducts()
+        {
+            string sql = @"SELECT Product.*, Manufacturer.Name Manufacturer, Category.name Category FROM Product INNER JOIN Manufacturer ON Product.IdManufacturer = Manufacturer.id INNER JOIN Category ON Product.IdCategory = Category.id";
+            using (var conn = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                return conn.Query<Product>(sql).ToList();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProduct(int id)
+        {
+            string sql = "SELECT * FROM Product WHERE id = @id";
+            using (var conn = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                return conn.QueryFirstOrDefault<Product>(sql, new { id });
+            }
+        }
+
+        [HttpPost("")]
+        public void CreateProduct(Product product)
+        {
+            string sql = @"INSERT INTO Product(
+            Name,Code,Price,IdManufacturer,IdCategory
+            ) OUTPUT inserted.id VALUES(
+            @Name,@Code,@Price,@IdManufacturer,@IdCategory
+            )";
+            using (var conn = new SqlConnection(configuration.GetConnectionString("connString")))
+            {
+                conn.Execute(sql, product);
+            }
+        }
+    }
+}
---
+++ OnlineshopWeb/DomainClasses.cs
@@ -0,0 +1,91 @@
+﻿namespace OnlineshopWeb
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Client
+    {
+        public int ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Code { get; set; }
+        public int? IdCompany { get; set; }
+    }
+    public class Company
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+    public class Invoice
+    {
+        public int ID { get; set; }
+        public string Number { get; set; }
+        public DateTime? DateIssued { get; set; }
+        public bool? Paid { get; set; }
+        public int? IdClient { get; set; }
+        public DateTime? DatePaid { get; set; }
+    }
+    public class InvoiceDetail
+    {
+        public int ID { get; set; }
+        public int? IdProduct { get; set; }
+        public int? Quantity { get; set; }
+        public int? IdInvoice { get; set; }
+        public decimal? UnitPrice { get; set; }
+    }
+    public class Manufacturer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime? DateOrdered { get; set; }
+        public DateTime? DateSent { get; set; }
+        public int? IdClient { get; set; }
+        public bool? Cancelled { get; set; }
+        public DateTime? DateProcessed { get; set; }
+        public int? IdEmployee { get; set; }
+    }
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public int? IdOrder { get; set; }
+        public int? IdProduct { get; set; }
+        public int? Quantity { get; set; }
+        public int? UnitPrice { get; set; }
+        public double? Rebate { get; set; }
+    }
+    public class Payment
+    {
+        public int Id { get; set; }
+        public DateTime? DateOfPayment { get; set; }
+        public decimal? Amount { get; set; }
+        public int? IdOrder { get; set; }
+    }
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public decimal? Price { get; set; }
+        public int? IdManufacturer { get; set; }
+        public int? IdCategory { get; set; }
+        public string Manufacturer { get; set; }
+        public string Category { get; set; }
+    }
+}
